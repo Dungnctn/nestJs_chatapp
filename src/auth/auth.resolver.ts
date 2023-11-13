01f8daabf461a6753da4f0362401422a676725d6
Chi Dung <Dungnc0402@gmail.com>
2023-11-13 17:18:52 +0700

    update profile user

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,7 +1,53 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { LoginResponse, RegisterResponse } from './types';
+import { LoginDto, RegisterDto } from './dto';
+import { Request, Response } from 'express';
+import { BadRequestException } from '@nestjs/common';
 
 @Resolver()
 export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => RegisterResponse)
+  //@Mutation => create, update, delete,...
+  async register(
+    @Args() registerDto: RegisterDto,
+    @Context() context: { res: Response },
+    //@Args => argument (su dung registerDto )
+    //@Context => ghi chu (annotation)
+  ) {
+    if (registerDto.password !== registerDto.confirmPassword) {
+      throw new BadRequestException({
+        confirmPassword: 'Password and confirm password not the same',
+      });
+    }
+    const { user } = await this.authService.register(registerDto, context.res);
+    return { user };
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Args() loginDto: LoginDto,
+    @Context() context: { res: Response },
+  ) {
+    return await this.authService.login(loginDto, context.res);
+  }
+
+  @Mutation(() => String)
+  async logout(@Context() context: { res: Response }) {
+    return await this.authService.logout(context.res);
+  }
+
+  @Mutation(() => String)
+  async refreshToken(@Context() context: { req: Request; res: Response }) {
+    try {
+      return await this.authService.refreshToken(context.req, context.res);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
   @Query(() => String)
   async hello() {
     return 'hello';
